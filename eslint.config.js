2b7b852e47a66e5aae00952c2d21a8431f796fe4
Phillip Couto <pcouto@eckler.ca>
2026-01-10 08:47:02 -0500

    chore: updated to latest nx v20

--- eslint.config.js
+++ eslint.config.js
@@ -41,7 +41,7 @@ module.exports = [
   },
   ...compat.config({ extends: ['plugin:@nx/typescript'] }).map((config) => ({
     ...config,
-    files: ['**/*.ts', '**/*.tsx'],
+    files: ['**/*.ts', '**/*.tsx', '**/*.cts', '**/*.mts'],
     rules: {
       ...config.rules,
       '@stylistic/no-extra-semi': 'error',
@@ -49,7 +49,7 @@ module.exports = [
   })),
   ...compat.config({ extends: ['plugin:@nx/javascript'] }).map((config) => ({
     ...config,
-    files: ['**/*.js', '**/*.jsx'],
+    files: ['**/*.js', '**/*.jsx', '**/*.cjs', '**/*.mjs'],
     rules: {
       ...config.rules,
       '@stylistic/no-extra-semi': 'error',
